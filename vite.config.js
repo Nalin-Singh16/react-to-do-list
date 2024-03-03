756cb852e816e4deb6620b36d8f6c768477431c5
Nalin-Singh16 <nalin.singh16@gmail.com>
2024-03-03 23:02:03 +0530

    Third commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Set the output directory to "build"
+  },
 });
